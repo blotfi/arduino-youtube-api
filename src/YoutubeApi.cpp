8c2ca3f3f4ef0d7fab495091896da37f1ee82c1b
Lotfi <24873186+blotfi@users.noreply.github.com>
2018-03-04 01:47:49 +0100

    update to display Video Stat
    Added getVideoStatistics(String videoId)

--- src/YoutubeApi.cpp
+++ src/YoutubeApi.cpp
@@ -17,7 +17,7 @@
    License along with this library; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
    
-   updated by blotfi
+   updated by blotfi / forked from witnessmenow/arduino-youtube-api
  */
 
 #include "YoutubeApi.h"
